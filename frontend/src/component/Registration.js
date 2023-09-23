519ed9e99b6bfa754423b27119857b0997e36419
Danil Egorkin <danilegorkin@Danils-Air.himki.net>
2023-09-23 12:55:08 +0300

    add func registration page

--- frontend/src/component/Registration.js
+++ frontend/src/component/Registration.js
@@ -1,5 +1,5 @@
 import '../App.css'
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from '@react-navigation/native';
 import Row from 'react-bootstrap/Row';
 import Box from '@mui/material/Box';
@@ -8,8 +8,35 @@ import Wrapper from '../wrapper.png';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios'
 
 function Registration ({navigation}){
+    const [formData, setFormData] = useState({
+        fio: '',
+        login: '',
+        password: '',
+        password2: '',
+        email: '',
+        sex: 'True',
+        birthdate: '2023-04-23'
+    });
+    const handleInputChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('http://localhost:8000/register', formData).then(response =>{
+            console.log(response.data);
+        })
+        .catch(error =>{
+            console.log(error);
+        });
+    };
+    console.log(formData);
     return (
         <Container fluid="md" className='ContainerAuth'>
             <Box
@@ -39,29 +66,35 @@ function Registration ({navigation}){
                     </Link>
                 </Row>
                 <Row>
-                    <Form>
+                    <Form id='myForm' onSubmit={handleSubmit}>
                         <Form.Group className='FormGroupInp' controlId="exampleForm.ControlInput1">
                             <Row className='RowInputs'>
                                 <Form.Label className='EmailText'>Email</Form.Label>
-                                <Form.Control className='formRow' type="email" placeholder="Email" />
+                                <Form.Control className='formRow' type="email" name='email' placeholder="Email" value={formData.email} onChange={handleInputChange}/>
                             </Row> 
                         </Form.Group>
                         <Form.Group className='FormGroupInp' controlId="formBasicPassword">
                             <Row className='RowInputs'>
                                 <Form.Label className='PassText'>Name</Form.Label>
-                                <Form.Control className='formRow' type="name" placeholder="Name" />
+                                <Form.Control className='formRow' type="name" name='fio' placeholder="Name" value={formData.fio} onChange={handleInputChange}/>
+                            </Row>  
+                        </Form.Group>
+                        <Form.Group className='FormGroupInp' controlId="formBasicPassword">
+                            <Row className='RowInputs'>
+                                <Form.Label className='PassText'>Login</Form.Label>
+                                <Form.Control className='formRow' type="name" name='login' placeholder="Login" value={formData.login} onChange={handleInputChange}/>
                             </Row>  
                         </Form.Group>
                         <Form.Group className='FormGroupInp' controlId="formBasicPassword">
                             <Row className='RowInputs'>
                                 <Form.Label className='PassText'>Password</Form.Label>
-                                <Form.Control className='formRow' type="password" placeholder="Password" />
+                                <Form.Control className='formRow' type="password" name='password' placeholder="Password" value={formData.password} onChange={handleInputChange}/>
                             </Row>  
                         </Form.Group>
                         <Form.Group className='FormGroupInp' controlId="formBasicPassword">
                             <Row className='RowInputs'>
                                 <Form.Label className='PassText'>Repeat password</Form.Label>
-                                <Form.Control className='formRow' type="password" placeholder="Password" />
+                                <Form.Control className='formRow' type="password" name='password2' placeholder="Password" value={formData.password2} onChange={handleInputChange}/>
                             </Row>  
                         </Form.Group>
                         <Row>
